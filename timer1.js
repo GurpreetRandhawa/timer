664082620511689c8e444e35032dc027fe45eb94
GurpreetRandhawa <singhpreet01991@gmail.com>
2023-02-23 01:31:26 -0800

    edge case

--- timer1.js
+++ timer1.js
@@ -2,7 +2,7 @@ let inputNumbers = process.argv.slice(2);
 inputNumbers.forEach((element, index) => {
   inputNumbers[index] = Number(element);
 });
-let res = inputNumbers.filter((element) => element).sort();
+let res = inputNumbers.filter((element) => element && element > 0).sort();
 if (res.length > 0) {
   for (const i of res) {
     setTimeout(() => {
